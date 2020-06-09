9b9f8b74e0d7da6a3b1747d74c9adcc78f3a52db
Fraser <775431157@qq.com>
2020-06-09 17:40:31 +0800

    serverless lab6

---
+++ lambda-sqs-lambda/README.md
@@ -0,0 +1,22 @@
+# Lab6 E-Commerce
+
+## How to Deploy
+
+```
+$ npm install
+$ npm run deploy
+```
+
+## Postman API Gateway 请求
+```
+POST https://xxxx.execute-api.ap-northeast-1.amazonaws.com/dev/order
+```
+
+request body:
+```
+{
+	"email": "xxxx@126.com",
+	"content": "订单已提交成功"
+}
+```
+
---
+++ lambda-sqs-lambda/app/postman.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const config = require('../conf/config.json')
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+const { v4: uuidv4 } = require('uuid');
+
+module.exports.sender = async (event, context, callback) => {
+    console.log(event)
+    let statusCode = 200
+    let message
+
+    if (!event.body) {
+        return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: 'No order body was found',
+        }),
+        };
+    }
+
+    const region = context.invokedFunctionArn.split(':')[3]
+    const accountId = context.invokedFunctionArn.split(':')[4]
+    const queueName = config['queueName']
+
+    const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`
+    const orderId = uuidv4()
+
+    try {
+        await sqs.sendMessage({
+            QueueUrl: queueUrl,
+            MessageBody: event.body,
+            MessageAttributes: {
+                orderId: {
+                    StringValue: orderId,
+                    DataType: 'String',
+                },
+            },
+        }).promise();
+
+        message = 'Order message is placed in the Queue!';
+
+  } catch (error) {
+    console.log(error);
+    message = error;
+    statusCode = 500;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify({
+      message, orderId,
+    }),
+  };
+};
